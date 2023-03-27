2d0f873147e406338881b44f71dc0964c7ecc564
NelsonMbogori <somimbogori@gmail.com>
2023-03-27 09:56:37 +0300

    react intro

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*components are js functions*/
+/*they return htm*/
+/*when installing libraries you dont need to specify path*/
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+/*when importing our own files you need to specify path*/
+import './index.css';//import css in js file
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function BookList(){
+    return <section className="booklist">
+        <Book/>
+        <Book/>
+        <Book/>
+        <Book/>
+    </section>
+}
+function Book(){
+    return <article>
+        <Image/>
+        <Title/>
+        <Author/>
+        
+    </article>
+}
+const Image  = ()=> <h2><img src="https://m.media-amazon.com/images/I/91zjmw1z7BL._AC_UL480_FMwebp_QL65_.jpg"/></h2>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Title  = ()=> <h2>Stolen Youth: How Radicals Are Erasing Innocence and Indoctrinating a Generation</h2>
+
+const Author  = ()=> <h2>Karol Markowicz and Bethany Mandel</h2>
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<BookList/>)
