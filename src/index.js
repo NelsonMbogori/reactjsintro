40b5346e15d5d17a1be606829abf1921ea4adc17
NelsonMbogori <somimbogori@gmail.com>
2023-03-27 10:45:16 +0300

    modified:   src/index.css 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 /*when importing our own files you need to specify path*/
+/*local imgaes should be in the src folder*/
+/* when writing css within the js file use the style attribute and then provide a json object
+eg <h4 style={{color:'black'}}></h4>
+*/ 
 import './index.css';//import css in js file
 
 function BookList(){
@@ -11,21 +15,23 @@ function BookList(){
         <Book/>
         <Book/>
         <Book/>
-        <Book/>
+        
     </section>
 }
+const Title = 'Stolen Youth: How Radicals Are Erasing Innocence and Indoctrinating a Generation';
 function Book(){
-    return <article>
-        <Image/>
-        <Title/>
-        <Author/>
-    </article>
-}
+    return (
+        <article className="book">
+            <Image/>
+            <h2>{Title}</h2>
+            <Author/>
+        </article>
+    );
+};
 const Image  = ()=> <h2><img src="https://m.media-amazon.com/images/I/91zjmw1z7BL._AC_UL480_FMwebp_QL65_.jpg"/></h2>
 
-const Title  = ()=> <h2>Stolen Youth: How Radicals Are Erasing Innocence and Indoctrinating a Generation</h2>
 
-const Author  = ()=> <h2>Karol Markowicz and Bethany Mandel</h2>
+const Author  = ()=> <h4>Karol Markowicz and Bethany Mandel</h4>
 
 
 
